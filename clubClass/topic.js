d2789f7b60d31b4421dd977a62d2e2ac0cdb21c8
ryankert01 <ryan980053@gmail.com>
2022-03-09 14:17:40 +0800

    nnothing changed

--- clubClass/topic.js
+++ clubClass/topic.js
@@ -7,11 +7,12 @@ var topic = [
 ];
 
 var startDate = new Date();
-function setMonthAndDay(startMonth, startDay){
-//一次設定好月份與日期
-startDate.setMonth(startMonth-1,startDay);
-startDate.setHours(0);
-startDate.setMinutes(0);
-startDate.setSeconds(0);
+
+function setMonthAndDay(startMonth, startDay) {
+    //一次設定好月份與日期
+    startDate.setMonth(startMonth - 1, startDay);
+    startDate.setHours(0);
+    startDate.setMinutes(0);
+    startDate.setSeconds(0);
 }
-setMonthAndDay(2,21);
+setMonthAndDay(2, 21);
